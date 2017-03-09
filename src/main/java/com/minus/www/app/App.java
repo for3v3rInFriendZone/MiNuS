eab314ab7987419487278edc6333c4f1cf124f4c
Martel <strile93@gmail.com>
2017-03-09 17:34:58 +0100

    Initial commit

---
+++ src/main/java/com/minus/www/app/App.java
@@ -0,0 +1,16 @@
+package com.minus.www.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
